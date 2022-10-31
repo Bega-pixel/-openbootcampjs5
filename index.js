c04b24740cb6808d9fddc0b1224bb0d4fff8aac4
Bega-pixel <bega2000@protonmail.com>
2022-10-31 08:55:11 +0100

    ejer5

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const altura =188 ;
-let  alturaMetros = 1.88 ;
+let  alturaMetros = altura * 0.01;
 const peso = 82;
 let alturaRedondeada = alturaRedondeada.Math.ceil(altura);
 let pesoRedondeado = pesoRedondeado.Math.flor(peso) ;
@@ -8,7 +8,7 @@ const maxValor = num.MAX_VALUE + 1 === num.MAX_VALUE;
 
 let num = 1;
 console.log(altura + " cm");
-console.log(alturaMetros + " metros");
+console.log(alturaMetros.toFixed(2) + " metros");
 console.log(peso + "kg");
 console.log(alturaRedondeada + "m");
 console.log(pesoRedondeado + "kg");
