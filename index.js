1c756d0913efbf8fdd7b871bb4a352db23e28247
Bega-pixel <bega2000@protonmail.com>
2022-10-31 08:46:34 +0100

    ejer5

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const altura =188 ;
+let  alturaMetros = 1.88 ;
+const peso = 82;
+let alturaRedondeada = alturaRedondeada.Math.ceil(altura);
+let pesoRedondeado = pesoRedondeado.Math.flor(peso) ;
+const maxValor = num.MAX_VALUE + 1 === num.MAX_VALUE;
+
+let num = 1;
+console.log(altura + " cm");
+console.log(alturaMetros + " metros");
+console.log(peso + "kg");
+console.log(alturaRedondeada + "m");
+console.log(pesoRedondeado + "kg");
+console.log(maxValor);
+
+
+
